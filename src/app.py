b0cce6f6aa4dae46fbf6a32b4667b5072fa5b1ea
Ramon Perez <ramonpzg@protonmail.com>
2023-06-21 13:04:21 +1000

    adding app and reqs

--- src/app.py
+++ src/app.py
@@ -1,13 +1,9 @@
 import streamlit as st
-from diffusers import AudioLDMPipeline
-import torch
 from qdrant_client import QdrantClient
-from qdrant_client.http.models import Distance, VectorParams
-from qdrant_client.http import models
-import pandas as pd
-from transformers import AutoModel, pipeline, AutoFeatureExtractor
-
-
+from transformers import pipeline
+from audiocraft.models import MusicGen
+from panns_inference import AudioTagging
+import os
 
 st.title("Music Recommendation App")
 st.subheader("A :red[Generative AI]-to-Real Music Approach")
@@ -20,108 +16,77 @@ There are several moving parts to this app and the most important ones are `tran
 Qdrant for our vector database.
 """)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-repo_id = "cvssp/audioldm"
-pipe = AudioLDMPipeline.from_pretrained(repo_id)
-# pipe = pipe.to("cuda")
+client     = QdrantClient(
+    "https://394294d5-30bb-4958-ad1a-15a3561edce5.us-east-1-0.aws.cloud.qdrant.io:6333", 
+    api_key=os.environ['QDRANT_API_KEY'],
+)
+classifier = pipeline("audio-classification", model="models/mod_checkpoints/checkpoint-90")#.to(device)
+model      = MusicGen.get_pretrained('small')
 
+# param1, param2 = st.columns(2)
+val1 = st.slider("How many seconds?", 5.0, 120.0, value=5.0, step=0.5)
+# val2 = param2.slider(
+#     "How many inference steps?", 5, 100, value=5, 
+#     help="The higher the number, the better the quality of the sound but the longer it takes for your music to be generated."
+# )
 
-param1, param2 = st.columns(2)
-val1 = param1.slider("How many seconds?", 5.0, 120.0, value=5.0, step=0.5)
-val2 = param2.slider(
-    "How many inference steps?", 5, 100, value=5, 
-    help="The higher the number, the better the quality of the sound but the longer it takes for your music to be generated."
+model.set_generation_params(
+    use_sampling=True,
+    top_k=250,
+    duration=val1
 )
 
 music_prompt = st.text_input(
     label="Music Prompt",
-    value="Techno music with a strong, upbeat tempo and high melodic riffs."
+    value="Fast-paced bachata in the style of Romeo Santos."
 )
 
+if st.button("Generate Some Music!"):
+    with st.spinner("Wait for it..."):
+        output = model.generate(descriptions=[music_prompt],progress=True)[0, 0, :].cpu().numpy()
+        st.success("Done! :)")
 
-audio = pipe(
-    prompt=music_prompt, num_inference_steps=val2, audio_length_in_s=val1
-).audios[0]
+    st.audio(output, sample_rate=32000)
 
-st.audio(audio, sample_rate=16000)
+    genres = classifier(output)
 
-classifier = pipeline("audio-classification", model="models/my_model")
+    if genres:
+        st.markdown("## Best Prediction")
+        col1, col2 = st.columns(2, gap="small")
+        col1.subheader(genres[0]['label'])
+        col2.metric(label="Score", value=f"{genres[0]['score']*100:.2f}%")
 
-genre = classifier(audio)
+        st.markdown("### Other Predictions")
+        col3, col4 = st.columns(2, gap="small")
+        for idx, genre in enumerate(genres[1:]):
+            if idx % 2 == 0:
+                col3.metric(label=genre['label'], value=f"{genre['score']*100:.2f}%")
+            else:
+                col4.metric(label=genre['label'], value=f"{genre['score']*100:.2f}%")
 
-st.markdown("## Best Prediction")
-m1, m2, m3, m4, m5 = st.columns(5)
+    at = AudioTagging(checkpoint_path=None)
+    clipwise_output, embedding = at.inference(output[None, :])
 
-m1.metric(label=genre[0]['label'], value=f"{genre[0]['score']*100:.2f}%")
-m2.metric(label=genre[1]['label'], value=f"{genre[1]['score']*100:.2f}%")
-m3.metric(label=genre[2]['label'], value=f"{genre[2]['score']*100:.2f}%")
-m4.metric(label=genre[3]['label'], value=f"{genre[3]['score']*100:.2f}%")
-m5.metric(label=genre[4]['label'], value=f"{genre[4]['score']*100:.2f}%")
+    vectr = embedding[0]
 
+    results = client.search(
+        collection_name="music_vectors",
+        query_vector=vectr,
+        limit=10
+    )
 
-model = AutoModel.from_pretrained('models/my_model').to(device)
-feature_extractor = AutoFeatureExtractor.from_pretrained("models/my_model")
-inputs = feature_extractor(
-    audio, sampling_rate=feature_extractor.sampling_rate, 
-    return_tensors="pt", max_length=16000, truncation=True
-)
+    st.markdown("## Real Recommendations")
 
-with torch.no_grad():
-    last_hidden_state = model(**inputs.to(device)).last_hidden_state[:, 0]
-last_hidden_state.size()
+    col5, col6 = st.columns(2)
 
-
-st.markdown("## Real Recommendations")
-
-client = QdrantClient("localhost", port=6333)
-vectr = last_hidden_state.cpu().numpy()[0, :]
-
-results = client.search(
-    collection_name="test_collection",
-    query_vector=vectr,
-    limit=10
-)
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.header(f"Genre: {results[0].payload['genre']}")
-    st.subheader(f"Artist: {results[0].payload['artist']}")
-    st.audio(results[0].payload["audio_path"])
-    
-    st.header(f"Genre: {results[1].payload['genre']}")
-    st.subheader(f"Artist: {results[1].payload['artist']}")
-    st.audio(results[1].payload["audio_path"])
-    
-    st.header(f"Genre: {results[2].payload['genre']}")
-    st.subheader(f"Artist: {results[2].payload['artist']}")
-    st.audio(results[2].payload["audio_path"])
-    
-    st.header(f"Genre: {results[3].payload['genre']}")
-    st.subheader(f"Artist: {results[3].payload['artist']}")
-    st.audio(results[3].payload["audio_path"])
-    
-    st.header(f"Genre: {results[4].payload['genre']}")
-    st.subheader(f"Artist: {results[4].payload['artist']}")
-    st.audio(results[4].payload["audio_path"])
-
-with col2:
-    st.header(f"Genre: {results[5].payload['genre']}")
-    st.subheader(f"Artist: {results[5].payload['artist']}")
-    st.audio(results[5].payload["audio_path"])
-    
-    st.header(f"Genre: {results[6].payload['genre']}")
-    st.subheader(f"Artist: {results[6].payload['artist']}")
-    st.audio(results[6].payload["audio_path"])
-    
-    st.header(f"Genre: {results[7].payload['genre']}")
-    st.subheader(f"Artist: {results[7].payload['artist']}")
-    st.audio(results[7].payload["audio_path"])
-    
-    st.header(f"Genre: {results[8].payload['genre']}")
-    st.subheader(f"Artist: {results[8].payload['artist']}")
-    st.audio(results[8].payload["audio_path"])
-    
-    st.header(f"Genre: {results[9].payload['genre']}")
-    st.subheader(f"Artist: {results[9].payload['artist']}")
-    st.audio(results[9].payload["audio_path"])
+    for idx, result in enumerate(results):
+        if idx % 2 == 0:
+            col5.header(f"Genre: {result.payload['genre']}")
+            col5.markdown(f"### Artist: {result.payload['artist']}")
+            col5.markdown(f"#### Song name: {result.payload['name']}")
+            col5.audio(result.payload["urls"])
+        else:
+            col6.header(f"Genre: {result.payload['genre']}")
+            col6.markdown(f"### Artist: {result.payload['artist']}")
+            col6.markdown(f"#### Song name: {result.payload['name']}")
+            col6.audio(result.payload["urls"])
